64f132f387e5b49d83db041ad0f313d37e26c659
alexmuriuki <alexmuriukimaina254@gmail.com>
2022-01-02 00:31:46 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const jokes = require("./jokes")
+const mathLib = require("./mathLib/math")
+
+const app = {}
+
+app.config = {
+  timeBetweenJokes: 1000,
+}
+
+app.printAJoke = () => {
+  const allJokes = jokes.allJokes()
+  const numberOfJokes = allJokes.length
+
+  const randomNumber = mathLib.getRandomNumber(1, numberOfJokes)
+
+  const selectedJoke = allJokes[randomNumber - 1]
+
+  console.log(selectedJoke)
+}
+
+app.indefiniteLoop = () => {
+  setInterval(app.printAJoke, app.config.timeBetweenJokes)
+}
+
+app.indefiniteLoop()
